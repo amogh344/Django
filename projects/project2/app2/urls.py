9bb923b5b64bf411d012ce47f969c953d7ea3436
Amogh Brahma R <142930584+amogh344@users.noreply.github.com>
2025-07-18 15:40:14 +0000

    changes made

---
+++ projects/project2/app2/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('greet/', views.greet, name='greet'),
+    path('', views.landing_page_view, name='landingpage'),
+    path('pricing/', views.pricing_page_view, name='pricing'),
+    path('contact/', views.contact_page_view, name='contact'),
+    path('student/', views.StudentInfoView, name='student'),
+]
--- projects/project2/app2/views.py
+++ projects/project2/app2/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from .forms import StudentForm, ContactForm
+from django.http import HttpResponse
 
-# Create your views here.
+def greet(request):
+    return HttpResponse("Hello from greet view!")
+
+users = [
+    {'name': 'Amogh', 'age': 21},
+    {'name': 'Brahma', 'age': 21},
+    {'name': 'Joe Goldberg', 'age': 21},
+]
+
+cards = [
+    {'title': 'Personal', 'description': 'This is a personal plan', 'original_price': 100, 'updated_price': 110},
+    {'title': 'Starter', 'description': 'This is a Starter plan', 'original_price': 100, 'updated_price': 50},
+    {'title': 'Advanced', 'description': 'This is an Advanced plan', 'original_price': 100, 'updated_price': 1110},
+]
+
+
+def student_info_view(request):
+    form = StudentForm()
+    return render(request, 'Student_info.html', {'form': form})
+
+def landingpage(request):
+    return render(request, 'landingpage.html')
+
+
+def pricing_page_view(request):
+    return render(request, 'pricingPage.html', {'cards': cards})
+
+
+def contact_page_view(request):
+    if request.method == 'POST':
+        f = ContactForm(request.POST)
+        if f.is_valid():
+            print("Your request is recorded")
+        else:
+            print("Your request is not recorded")
+        return render(request, 'contact.html', {'form': f})
+    else:
+        f = ContactForm()
+        return render(request, 'contact.html', {'form': f})

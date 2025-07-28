ce4b3eaec4aa57ea81a6a823b20f6a408c48c434
Amogh Brahma R <142930584+amogh344@users.noreply.github.com>
2025-07-28 15:04:20 +0000

    modified

--- projects/project3/app3/urls.py
+++ projects/project3/app3/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    # path('greet/', views.greet, name='greet'),
     path('', views.landing, name='landing'),
     path('pricing/', views.pricing, name='pricing'),
     path('contact/', views.contact, name='contact'),
